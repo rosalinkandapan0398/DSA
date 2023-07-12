fc89609cccec11033919c78371f590988331f412
Rosalin <rosalinkandapan0398@gmail.com>
2023-07-13 00:25:58 +0530

    Third Commit

---
+++ LongestCommonPrefix_Leetcode.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix_Leetcode {
+	public static String longestCommonPrefix(String[] strs) {
+		Arrays.sort(strs);
+        String s1=strs[0];
+         String s2=strs[strs.length-1];
+		int idx=0;
+      while(idx<s1.length() && idx<s2.length()){
+          if(s1.charAt(idx)==s2.charAt(idx)){
+              idx++;
+          }
+          else{
+              break;
+          }
+      }
+      return s1.substring(0, idx);
+		
+        }
+	
+	public static void main(String[] args) {
+		String[]strs =new  String[] {"flower","flow","flight"};
+		longestCommonPrefix(strs);
+	}
+
+}
+
+
